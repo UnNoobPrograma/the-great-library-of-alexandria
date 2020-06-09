35e65859304822c74fd3c1a86b20bc7c5961f5fa
Link Strifer <linkstrifer@users.noreply.github.com>
2020-06-09 02:53:33 +0000

    Automated commit: Generated doc files

---
+++ docs/build/precache-manifest.c3ef40cd0854a0d65b0cebd2b10b9ff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13c1f42473311e70146f97f503173f0",
+    "url": "/the-great-library-of-alexandria/index.html"
+  },
+  {
+    "revision": "eacb5f9624b9538f39a6",
+    "url": "/the-great-library-of-alexandria/static/css/main.27df1849.chunk.css"
+  },
+  {
+    "revision": "10de4eb31fa186f031a0",
+    "url": "/the-great-library-of-alexandria/static/js/2.657eba9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-great-library-of-alexandria/static/js/2.657eba9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eacb5f9624b9538f39a6",
+    "url": "/the-great-library-of-alexandria/static/js/main.acb6a875.chunk.js"
+  },
+  {
+    "revision": "708270fc492e26eaceeb",
+    "url": "/the-great-library-of-alexandria/static/js/runtime-main.86f9517d.js"
+  }
+]);
