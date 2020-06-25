3aa18ce8d60aa42fd7be6eb348eeff7561af417b
Link Strifer <linkstrifer@outlook.com>
2020-06-24 21:41:24 -0500

    add multiple description support, add some terms

--- src/components/Term/index.js
+++ src/components/Term/index.js
@@ -27,9 +27,13 @@ export default function Item({
   return (
     <div onClick={changeLocation} id={hash.toLowerCase()} className={container}>
       <dt className={titleClass}>
-        {abbr ? <abbr title={title}>{abbr}</abbr> : title}
+        <h2>{abbr ? <abbr title={title}>{abbr}</abbr> : title}</h2>
       </dt>
-      <dd className={descriptionClass}>{description}</dd>
+      <dd className={descriptionClass}>
+        {Array.isArray(description)
+          ? description.map((description) => <p>{description}</p>)
+          : description}
+      </dd>
       {resources.length > 0 && (
         <details className={detailsClass}>
           <summary className={summaryClass}>Recursos:</summary>
